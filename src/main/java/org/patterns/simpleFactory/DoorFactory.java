5f03364304f867c5c69e99c077336b0b469275bc
Aniket Patil <aniketpatil5687@gmail.com>
2024-09-02 01:48:32 -0500

    Simple Factory design patten code added

---
+++ src/main/java/org/patterns/simpleFactory/DoorFactory.java
@@ -0,0 +1,8 @@
+package org.patterns.simpleFactory;
+
+public class DoorFactory {
+
+    public static Door makeDoor(float width, float height){
+        return new WoodenDoor(width,height);
+    }
+}
