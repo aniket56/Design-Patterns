92aa2109ae8b1e6e457502aaea9d1c8758a7fc82
Aniket Patil <aniketpatil5687@gmail.com>
2024-09-02 00:54:43 -0500

    Initial Commit

---
+++ src/main/java/org/patterns/simpleFactory/Door.java
@@ -0,0 +1,7 @@
+package org.patterns.simpleFactory;
+
+public interface Door {
+
+    float getWidth();
+    float getHeight();
+}
---
+++ src/main/java/org/patterns/simpleFactory/WoodenDoor.java
@@ -0,0 +1,15 @@
+package org.patterns.simpleFactory;
+
+public class WoodenDoor implements Door{
+
+    private float width;
+    private float height;
+    public float getWidth() {
+        return 0;
+    }
+
+
+    public float getHeight() {
+        return 0;
+    }
+}
