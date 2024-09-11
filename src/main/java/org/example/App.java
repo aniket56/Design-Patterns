20eec404f9b5dcce46c6aee61646a6b63bf149c2
Aniket Patil <aniketpatil5687@gmail.com>
2024-09-11 16:07:11 -0500

    Interview questions added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.patterns.factoryMethod.Interviewer;
 import org.patterns.simpleFactory.Door;
 import org.patterns.simpleFactory.DoorFactory;
+
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.example.DinnerTable.count;
 
 /**
  * Hello world!
@@ -13,8 +24,20 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        Door door = DoorFactory.makeDoor(100,200);
+       /* Door door = DoorFactory.makeDoor(100,200);
         System.out.println("Widht: "+door.getWidth());
-        System.out.println("Height: "+door.getHeight());
+        System.out.println("Height: "+door.getHeight());*/
+
+        System.out.println(count(10, 10, 3, 5)); // Example call
+
+
+        List<Integer> numbers = Arrays.asList(1,2,3,3,45,5);
+
+        numbers.stream().sorted().forEach(System.out::println);
+
+
+
     }
 }
+
+
